77547158baacf9ac94b7cb2483b708695c5d7e18
joelsubash-g <joel.subash.g@gmail.com>
2023-11-29 12:55:41 +0530

    initial commit

---
+++ setup/s3.py
@@ -0,0 +1,22 @@
+import boto3
+from secure import decrypt
+import os, traceback
+from dotenv import load_dotenv
+import ses
+
+load_dotenv()
+
+def upload_to_s3(key, bucket_name, file_name):
+
+    try:
+        AWS_SECRET_KEY = decrypt(os.environ.get("AWS_SECRET_KEY"), key)
+        AWS_ACCESS_KEY = decrypt(os.environ.get("AWS_ACCESS_KEY"), key)
+        s3_conn_obj = boto3.client('s3',
+                        aws_access_key_id = AWS_ACCESS_KEY,
+                        aws_secret_access_key = AWS_SECRET_KEY
+                        )
+        return s3_conn_obj.upload_file( file_name, bucket_name, file_name )
+    
+    except Exception as e:
+        print(e)
+        ses.send_email(key, subject='AWS S3 Connectivity/File Upload Error', message=traceback.format_exc())
